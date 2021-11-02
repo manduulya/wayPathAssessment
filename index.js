e791f1332db570d11ea8a0770f71905842b681fe
ebo <manduuyla@yahoo.com>
2021-11-01 21:44:47 -0400

    portrait responsive added

---
+++ index.js
@@ -0,0 +1,8 @@
+if (window.innerHeight > window.innerWidth) {
+    document.querySelector('#main-header').classList.add('portrait-header');
+    document.querySelector('nav').classList.add('portrait-nav');
+    document.querySelector('img').classList.add('portrait-img');
+    document.querySelector('#main-container').classList.add('portrait-container');
+    document.querySelector('#main-section').classList.add('portrait-section');
+    document.querySelector('button').classList.add('portrait-button');
+}
