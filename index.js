0dcf487d29c33096a286ac7fdf6d19c99ae2f729
ebo <manduuyla@yahoo.com>
2021-11-01 22:08:39 -0400

    portrait JS added

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ if (window.innerHeight > window.innerWidth) {
     document.querySelector('#main-container').classList.add('portrait-container');
     document.querySelector('#main-section').classList.add('portrait-section');
     document.querySelector('button').classList.add('portrait-button');
-}
+    document.querySelector('h1').classList.add('portrait-h1');
+    const remove1 = document.querySelector('#remove1');
+    remove1.remove();
+    const remove2 = document.querySelector('#remove2');
+    remove2.remove();
+    const remove3 = document.querySelector('#remove3');
+    remove3.remove();
+};
